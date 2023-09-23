4418328ff9c219b2f7fbc2c040d8988ce23f4887
GabrielZ1 <GabrielZ1@localhost>
2023-09-23 14:34:51 -0700

    Finished ArrayCheatSheet

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,13 +3,14 @@ package arrays;
 import java.util.Random;
 
 public class _00_ArrayCheatSheet {
+
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		String[] stringList = {"hello", "my", "name", "is", "gabriel"};
+		String[] stringList = {"one", "two", "three", "four", "five"};
 		//2. print the third element in the array
 		System.out.println(stringList[2]);
 		//3. set the third element to a different value
-		stringList[2] = "NAME";
+		stringList[2] = "THREE";
 		//4. print the third element again
 		System.out.println(stringList[2]);
 		//5. use a for loop to print all the values in the array
@@ -18,21 +19,31 @@ public class _00_ArrayCheatSheet {
 			System.out.println(stringList[i]);
 		}
 		//6. make an array of 50 integers
-		int[] intList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
-		//7. use a for loop to make every value of the integer array a random number
-
+		int[] intList = new int[50];
 		//this makes every integer in the array a random number from 0-99
 		for(int i = 0; i<intList.length; i++) {
 			intList[i] = new Random().nextInt(100);
 		}
 		//8. without printing the entire array, print only the smallest number in the array
-		System.out.println("Smallest number: " + );
+		int smallestNumber = intList[0];	
+		for(int i = 0; i<intList.length; i++) {
+			if(intList[i]<smallestNumber) {
+				smallestNumber = intList[i];
+			}
+		}
+		System.out.println("Smallest number: " + smallestNumber );
 		//9 print the entire array to see if step 8 was correct
 		for(int i = 0; i<intList.length; i++) {
 			System.out.println(intList[i]);
 		}
 		//10. print the largest number in the array.
-		System.out.println("Largest number: " + );
+		int largestNumber = intList[0];	
+		for(int i = 0; i<intList.length; i++) {
+			if(intList[i]>largestNumber) {
+				largestNumber = intList[i];
+			}
+		}
+		System.out.println("Largest number: " + largestNumber);
 
 	}
 }
